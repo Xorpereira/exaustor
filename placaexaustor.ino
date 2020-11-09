0230d7d5dc8f5c5f9d4e73329240cbae88287303
Xorpereira <32929861+Xorpereira@users.noreply.github.com>
2020-11-09 01:42:51 +0000

    Update placaexaustor.ino

--- placaexaustor.ino
+++ placaexaustor.ino
@@ -1,13 +1,5 @@
-
-void loop() {
-  if (digitalRead(2) == HIGH);
-  Serial.println("Sensor is touched");
-  delay(500);
-
-}
-
 // constants won't change
-const int RELAY_PIN1 = 3;  // the Arduino pin, which connects to the IN pin of relay
+const int RELAY_PIN1 = 3;
 const int RELAY_PIN2 = 4;
 const int RELAY_PIN3 = 5;
 const int RELAY_PIN4 = 6;
@@ -15,10 +7,11 @@ const int ONOFF = 7;
 const int MORE = 8;
 const int LESS = 9;
 const int LIGHTONOFF =10;
+const int onoffstate = 0;
+const int strength = 1;
+const int lightstate = 0;
 
-// the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin 9 as an output.
   pinMode(RELAY_PIN1, OUTPUT);
   pinMode(RELAY_PIN2, OUTPUT);
   pinMode(RELAY_PIN3, OUTPUT);
@@ -30,11 +23,63 @@ void setup() {
   Serial.begin(9600);
 }
 
-// the loop function runs over and over again forever
-void loop() {
-  digitalWrite(RELAY_PIN, HIGH);
-  delay(500);
-  digitalWrite(RELAY_PIN, LOW);
-  delay(500);
+void onofftoggle() {
+	if (digitalRead(ONOFF) == HIGH) {
+		if (onoffstate) == 0 {
+		onoffstate = 1;
+		digitalWrite(RELAY_PIN1, HIGH);
+		}
+	}
+    else {
+      onoffstate = 0;
+      digitalWrite(RELAY_PIN1, LOW);
+    }
 }
 
+void powerup() {
+	if (digitalRead(MORE) == HIGH) {
+		if (strength) < 3 {
+			strength = strength+1;
+			if(strength) == 2){
+				digitalWrite(RELAY_PIN2, HIGH);
+			}
+			else if(strength) == 3 {
+				digitalWrite(RELAY_PIN3, HIGH);
+			}
+		}
+	}	
+}
+
+void powerdown() {
+	if (digitalRead(MORE) == HIGH) {
+		if (strength) > 1 {
+			strength = strength-1;
+			if(strength) == 1){
+				digitalWrite(RELAY_PIN2, LOW);
+			}
+			else if(strength) == 2 {
+				digitalWrite(RELAY_PIN3, LOW);
+			}
+		}
+	}	
+}
+
+void togglelight() {
+	if (digitalRead(LIGHTONOFF) == HIGH) {
+		if (lightstate) == 0 {
+		lightstate = 1;
+		digitalWrite(RELAY_PIN4, HIGH);
+		}
+	}
+    else {
+      lightstate = 0;
+      digitalWrite(RELAY_PIN4, LOW);
+    }
+}
+
+void loop() {
+	onofftoggle();
+	powerup()
+	powerdown()
+	togglelight()
+}
